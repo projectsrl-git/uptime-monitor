20113b2fc941ba4847236435ff1a5769f06d142c
defendimattia <defendi.mattia92@gmail.com>
2026-07-21 12:17:37 +0200

    Add email notification functionality and update settings for email backend

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+
 
 # Application definition
 
--- notification/services/email_notifier.py
+++ notification/services/email_notifier.py
@@ -1,5 +1,33 @@
+import os
+
+from django.core.mail import send_mail
+
+
 class EmailNotifier:
 
     def send(self, event, incident):
-        # Implementeremo l'invio email più avanti
-        pass
+
+        recipient = os.getenv("NOTIFICATION_EMAIL")
+
+        if not recipient:
+            return
+
+        subject = f"Monitor {event.upper()}: " f"{incident.monitor.name}"
+
+        message = f"""
+Monitor:
+{incident.monitor.name}
+
+URL:
+{incident.monitor.url}
+
+Evento:
+{event.upper()}
+"""
+
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=None,
+            recipient_list=[recipient],
+        )
