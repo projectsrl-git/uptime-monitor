dfe1fc5df3f0d63b11689c2b22e6296631949726
defendimattia <defendi.mattia92@gmail.com>
2026-08-25 12:15:30 +0200

    Add duplicate monitor functionality and update monitor detail page layout

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import home, add_monitor_page,incidents_page,incident_detail, monitor_detail
+from .views import home, add_monitor_page,incidents_page,incident_detail, monitor_detail, duplicate_monitor
 
 urlpatterns = [
     path("", home, name="dashboard"),
     path("monitor/add/", add_monitor_page, name="add_monitor"),
     path("monitor/<int:id>/", monitor_detail, name="monitor_detail"),
+    path("monitor/<int:id>/duplicate/", duplicate_monitor, name="monitor_duplicate"),
     path("incidents/", incidents_page, name="incidents"),
     path("incidents/<int:id>/", incident_detail, name="incident_detail"),
 ]
